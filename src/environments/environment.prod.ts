1c2b2e2c7a76b3a22a9e6eab1869df6b856b8029
fabri <fabricioalama2001@outlook.es>
2025-02-13 23:42:47 -0500

    fix bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api.attendance.yavirac.edu.ec',
+  API_URL: 'https://api.attendance.yavirac.edu.ec/api/v1',
   APP_NAME:'Sistema Informático de Registro de Asistencia ',
   APP_SHORT_NAME:'SIC'
 };
