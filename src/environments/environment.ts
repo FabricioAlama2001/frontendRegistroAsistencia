eed7d5addf4a165975017b62436d5d74c12c5ef8
cmtl0204 <cesar.tamayo0204@gmail.com>
2025-02-26 08:50:15 -0500

    fix bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   API_URL: 'http://localhost:3000/api/v1',
-  APP_NAME:'Local - Sistema Informático de Registro de Asistencia ',
+  APP_NAME:'Local - Sistema Informático de Registro de Asistencia',
   APP_SHORT_NAME:'SIRA - Pruebas Local'
 };
